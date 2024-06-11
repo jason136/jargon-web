4d80b2a6d82f1fa05dda3c671ca27cdce9792697
David Bai <142951611+dav1dbai@users.noreply.github.com>
2024-06-11 09:51:40 -0700

    reset password func (#1)
    * reset password func
    
    * reset password func

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
+export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL ?? "",
   process.env.NEXT_PUBLIC_SUPABASE_KEY ?? "",
   {
