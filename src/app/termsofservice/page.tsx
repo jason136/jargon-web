5bdeeaafc57a548d173c7b8e6c9f0fa97f63bd4e
Jason Wiemels <jwiemels@usc.edu>
2024-06-11 21:26:18 -0700

    cleaning

--- src/app/termsofservice/page.tsx
+++ src/app/termsofservice/page.tsx
@@ -2,9 +2,9 @@ import React from "react";
 const pdfPath = "/termsofservice.pdf#zoom=100";
 
 export default function PrivacyPage() {
-    return (
-        <div className="flex items-center justify-center h-screen w-full bg-gray-100">
-            <iframe src={pdfPath} className="h-screen w-full"/>
-        </div>
-    );
+  return (
+    <div className="flex h-screen w-full items-center justify-center bg-gray-100">
+      <iframe src={pdfPath} className="h-screen w-full" />
+    </div>
+  );
 }
