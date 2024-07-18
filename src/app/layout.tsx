d3cb7cbde899db3b89ce1209eef8cb621e18d003
David Bai <142951611+dav1dbai@users.noreply.github.com>
2024-07-17 23:44:52 -0700

    added bottom links and updated policy (#11)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,11 +41,11 @@ export default function RootLayout({
             </div>
             <Link
               className="hidden flex-row items-center justify-center gap-2 rounded-[20px] bg-violet-700 px-6 py-2 sm:flex"
-              href="https://docs.google.com/forms/d/e/1FAIpQLSfOcYOZsjZXoeZE8HIHk7VW8bVGG6k4SjpuWFcQabo31zzotA/viewform"
+              href="https://chromewebstore.google.com/detail/jargon/gghkanaadhldgmknmgggdgfaonhpppoj?pli=1"
               target="_blank"
             >
               <div className="whitespace-nowrap text-nowrap text-[15px] font-semibold text-white">
-                Join Our Waitlist
+                Download Extension
               </div>
               <Image
                 src="/external_site.svg"
@@ -60,15 +60,57 @@ export default function RootLayout({
             {children}
           </div>
 
-          <div className="flex w-screen flex-row items-center justify-center bg-violet-200 px-20 py-8 sm:justify-between">
+          <div className="flex w-screen flex-row justify-center bg-violet-200 px-20 py-8 sm:justify-between">
             <Image
-              className="hidden sm:flex"
+              className="hidden sm:block sm:h-[91px] sm:w-[262px] object-contain"
               src="/jargon.png"
               alt="Jargon Logo"
               width={262}
               height={91}
             />
-            <div className="flex flex-col items-center gap-6 sm:flex-row">
+            <div className="flex flex-col items-center gap-6 flex-grow py-4">
+              <div className="text-[16px] text-indigo-700 font-medium">Community</div>
+              <Link
+                href="https://discord.com/invite/aSeHJw3Db4"
+                target="_blank">
+                  <div className="text-[14px] text-indigo-400 font-medium">Join the Discord!</div>
+              </Link>
+            </div>
+            <div className="flex flex-col items-center gap-6 flex-grow py-4">
+              <div className="text-[16px] text-indigo-700 font-medium">Support</div>
+              <Link
+                href="mailto:support@jargonlearn.com"
+                target="_blank">
+                  <div className="text-[14px] text-indigo-400 font-medium">Contact Us</div>
+              </Link>
+              <Link
+                href="/termsofservice"
+                target="_blank">
+                  <div className="text-[14px] text-indigo-400 font-medium">Terms of Service</div>
+              </Link>
+              <Link
+                href="/privacy"
+                target="_blank">
+                  <div className="text-[14px] text-indigo-400 font-medium">Privacy Policy</div>
+              </Link>
+            </div>
+            <div className="flex flex-col items-center gap-6">
+            <Link
+                className="flex flex-row items-center justify-center gap-1 rounded-[40px] bg-violet-700 px-6 py-2 sm:gap-2 sm:px-12 sm:py-4"
+                href="https://chromewebstore.google.com/detail/jargon/gghkanaadhldgmknmgggdgfaonhpppoj?pli=1"
+                target="_blank"
+              >
+                <div className="whitespace-nowrap text-nowrap text-[16px] font-medium text-white sm:text-[22px]">
+                  Download Extension
+                </div>
+                <Image
+                  className="h-[13px] w-[12px] object-contain sm:h-[15px] sm:w-[14px]"
+                  src="/external_site.svg"
+                  alt="External Link Icon"
+                  height={14}
+                  width={15}
+                />
+              </Link>
               <div className="flex flex-row gap-6">
                 <Link
                   href="https://www.instagram.com/jargonlearn"
@@ -104,23 +146,7 @@ export default function RootLayout({
                   />
                 </Link>
               </div>
-              <Link
-                className="flex flex-row items-center justify-center gap-1 rounded-[40px] bg-violet-700 px-6 py-2 sm:gap-2 sm:px-12 sm:py-4"
-                href="https://docs.google.com/forms/d/e/1FAIpQLSfOcYOZsjZXoeZE8HIHk7VW8bVGG6k4SjpuWFcQabo31zzotA/viewform"
-                target="_blank"
-              >
-                <div className="whitespace-nowrap text-nowrap text-[16px] font-medium text-white sm:text-[22px]">
-                  Join Our Waitlist
-                </div>
-                <Image
-                  className="h-[13px] w-[12px] object-contain sm:h-[15px] sm:w-[14px]"
-                  src="/external_site.svg"
-                  alt="External Link Icon"
-                  height={14}
-                  width={15}
-                />
-              </Link>
-            </div>
+              </div>
           </div>
         </main>
       </body>
